b4c31dbb6da02da4ccaad5a2b739dff3bebc5046
MuhammadHasan004 <hasanmuhammadhm1122@gmail.com>
2024-05-26 14:04:03 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var famous_person = "William Shakespeare,";
+var message = "".concat(famous_person, " onesaid To be or not to be, that is the question.");
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let famous_person:string="William Shakespeare,";
+let message:string=`${famous_person} onesaid To be or not to be, that is the question.`;	
+console.log(message);
